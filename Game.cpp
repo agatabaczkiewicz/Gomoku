ad72773c154a8643a807c05ed970490933a1c288
unknown <agata.baczkiewicz@gmail.com>
2019-11-24 22:07:59 +0100

    sprawdzanie wygranego i okienkinka popupowe

--- Game.cpp
+++ Game.cpp
@@ -21,3 +21,157 @@ Player* Game::get_player2()
 {
 	return this->player2;
 }
+
+bool Game::checkTheEnd()
+{
+	int** tab = this->matrix->get_matrix();
+	int x = this->matrix->where_moveX();
+	int y = this->matrix->where_moveY();
+	int counter = 1;	// counter of the linked stones
+	int* piece = &tab[x][y]; // the last placed piece to be checked
+
+	/* horizontal line stones count */
+	while (y > 0)	// counting backward 
+	{
+		y--;
+		if (tab[x][y] == *piece) {
+			counter++;	// if the left stone of the current are the same than p1, increment counter
+		}
+		else {
+			break;	// if the left stone of the current are different than p1, exit the loop 
+		}
+	}
+
+	if (counter >= 5) {
+		return true;	//check if there are already 5 stones in a line
+	}
+
+	y = this->matrix->where_moveY();	// reset the vertical axis 
+	while (y < 8)	// counting forward
+	{
+		y++;
+		if (tab[x][y] == *piece) {
+			counter++;	// if the right stone of the current are the same than p1, increment counter
+		}
+		else {
+			break;	// if the right stone of the current are different than p1, exit the loop 
+		}
+	}
+
+	if (counter >= 5) {
+		return true;
+	}
+	/* horizontal line stones count */
+
+	/* vertical line stones count */
+	counter = 1;	// reset the counter
+	y = this->matrix->where_moveY();	// reset the vertical axis 
+	while (x > 0)	// counting upward 
+	{
+		x--;
+		if (tab[x][y] == *piece) {
+			counter++;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (counter >= 5) {
+		return true;
+	}
+
+	x = this->matrix->where_moveX();	// reset the axis 
+	while (x < 8)	// counting downward
+	{
+		x++;
+		if (tab[x][y] == *piece) {
+			counter++;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (counter >= 5) {
+		return true;
+	}
+	/* vertical line stones count */
+
+	/* left diagonal lines stones count */
+	counter = 1;	// reset the counter
+	x = this->matrix->where_moveX();	// reset the axis 
+	while (x > 0 && y > 0)	// counting left upper incline 
+	{
+		x--;
+		y--;
+		if (tab[x][y] == *piece) {
+			counter++;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (counter >= 5) {
+		return true;
+	}
+
+	x = this->matrix->where_moveX();	// reset the axis
+	y = this->matrix->where_moveY();	// reset the vertical axis
+	while (x < 8 && y <8)	// counting downward
+	{
+		x++;
+		y++;
+		if (tab[x][y] == *piece) {
+			counter++;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (counter >= 5) {
+		return true;
+	}
+	/* left diagonal line stones count */
+
+	/* right diagonal lines stones count */
+	counter = 1;	// reset the counter
+	x = this->matrix->where_moveX();	// reset the axis
+	y = this->matrix->where_moveY();	// reset the vertical axis
+	while (y > 0 && x < 8)	// counting left
+	{
+		x++;
+		y--;
+		if (tab[x][y] == *piece) {
+			counter++;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (counter >= 5) {
+		return true;
+	}
+
+	x = this->matrix->where_moveX();	// reset the axis
+	y = this->matrix->where_moveY();	// reset the vertical axis
+	while (x > 0 && y < 8)	// counting right
+	{
+		x--;
+		y++;
+		if (tab[x][y] == *piece) {
+			counter++;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (counter >= 5) {
+		return true;
+	}
+	return false;
+}
